1aae589c9a61d8f2c710bf7dc6fc867f0feeb007
kamalpokharel <pokharelikamal@gmail.com>
2024-09-17 10:34:18 +0545

    Initial commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class DemoApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // A simple method to be tested
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/example/DemoApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class DemoApplicationTest {
+
+    @Test
+    public void testAdd() {
+        DemoApplication app = new DemoApplication();
+        int result = app.add(2, 2);
+        assertEquals(5, result);
+    }
+}
