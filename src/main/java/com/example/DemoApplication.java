cd08429d9242c307dbe56540f611ad683cc3e43b
kamalpokharel <pokharelikamal@gmail.com>
2024-09-20 12:10:54 +0545

    test

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example;
 public class DemoApplication {
     // Method to add two numbers
     public int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public int sub(int a, int b) {
